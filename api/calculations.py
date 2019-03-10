72382d061a600d5163a2492f73fc60043989f8f1
kaijc <kai.j.chiem@gmail.com>
2019-03-10 02:02:25 +0100

    calc

--- api/calculations.py
+++ api/calculations.py
@@ -1,7 +1,10 @@
 from mock import mock_normal_consumption
 import numpy as np
 
-def calculate_saving(data):
+def calculate_saving(data, totalAmount=1000.0, date="2019-03-10"):
+
+    print("\n\n",totalAmount, date, "\n\n")
+
     normal_consumption = mock_normal_consumption()
     categories = data["children"]
 
@@ -25,21 +28,23 @@ def compare_spending_habits(category_name, normal_spending, customer_spending):
     normal_children = normal_spending["children"]
     customer_children = customer_spending["children"]
 
+    print(customer_children)
+
     # send in more data so it is possible to check if it has children
     # if it has children just compare the size
     if customer_children:
         for i in range(len(customer_children)):
             cat = {}
 
-            for j in range(len(normal_spending)):
+            for j in range(len(normal_children)):
                 if customer_children[i]["name"] == normal_children[j]["name"]:
                     cat["name"] = customer_children[i]["name"]
                     if customer_children[i]["size"] > normal_children[j]["size"]:
                         cat["size"] = abs(round(customer_children[i]["size"]-normal_children[j]["size"], 2))
                     else:
                         cat["size"] = 0.0
-                        children.append(cat)
-                        break
+                    children.append(cat)
+                    break
     else:
         if normal_spending["size"] < customer_spending["size"]:
             possible_saving["size"] =  abs(round(customer_spending["size"]-normal_spending["size"], 2))
@@ -66,5 +71,6 @@ def total_consumption(data):
             sub_category = category["children"]
             for j in range(len(sub_category)):
                 spendings += sub_category[j]["size"]
+
 
     return round(spendings, 2)
